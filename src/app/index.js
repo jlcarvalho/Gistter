b1d912387a7bfc9f3f6218f3df490f806a858229
Jean lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2015-03-11 11:44:08 -0300

    First commit

---
+++ src/app/index.js
@@ -0,0 +1,21 @@
+'use strict';
+/*jshint esnext: true */
+
+import MainCtrl from './main/main.controller';
+import NavbarCtrl from '../components/navbar/navbar.controller';
+
+angular.module('coder', ['restangular', 'ui.router', 'ngMaterial', 'ui.ace'])
+  .controller('MainCtrl', MainCtrl)
+  .controller('NavbarCtrl', NavbarCtrl)
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl as main'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
